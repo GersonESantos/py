94cd4cf96a36fa8819b8dc1ebb001798feb1a5d1
GersonESS <gebhsantos@gmail.com>
2025-05-02 13:52:45 -0300

    seg

---
+++ meuProjeto/scripts/teste01_pandas.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Criar DataFrame com mais colunas
+df = pd.DataFrame({
+    "Produto": ["A", "B", "C"],
+    "Preço": [10, 20, 30],
+    "Quantidade": [100, 50, 20]
+})
+
+# Salvar em CSV
+df.to_csv("dados/produtos.csv", index=False)
+
+# Carregar e mostrar
+df_lido = pd.read_csv("dados/produtos.csv")
+
+# Calcular valor total por produto (nova coluna)
+df_lido["Valor_Total"] = df_lido["Preço"] * df_lido["Quantidade"]
+
+# Mostrar resultados
+print(df_lido)
+
+# Salvar novamente com a nova coluna
+df_lido.to_csv("dados/produtos_atualizados.csv", index=False)
