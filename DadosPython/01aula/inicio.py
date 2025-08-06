ef6e4bbbd8503b8a37a1b5e260c5f9248489f1b8
GersonESS <gebhsantos@gmail.com>
2025-08-06 03:06:55 -0300

    Update inicio.py

--- DadosPython/01aula/inicio.py
+++ DadosPython/01aula/inicio.py
@@ -3,10 +3,10 @@ import numpy as np
 
 df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
 
-df.head(10)
-df.info()
-df.describe()
-
-df.isnull().sum()   
+print(df.head(10))
+print(df.info())
+print(df.describe())
+linhas, colunas = df.shape
+print(f"Linhas: {linhas} | Colunas: {colunas}")
+print(df.isnull().sum())
 df.dropna(inplace=True)
-df.isnull().sum()
